984d65179f0a4ca943bad247d6efd697e616f88a
Anupam Tripathi <tripathi.anupam1201@gmail.com>
2025-06-02 23:41:32 +0530

    Created an error class

--- src/controllers/airplane-controllers.js
+++ src/controllers/airplane-controllers.js
@@ -1,28 +1,27 @@
 const { response } = require('express');
 const { AirplaneService } = require('../services');
+
+const { SuccessResponse,ErrorResponse } = require('../utils/Common');
 const { StatusCodes } =  require('http-status-codes');
 
 //POST
 //req.body();
 
 async function createAirplane(req,res){
+
     try {
         const airplane = await AirplaneService.createAirplane({
             modelNumber : req.body.modelNumber,
             capacity : req.body.capacity
         });
-        return res.status(StatusCodes.CREATED).json({
-            success :  true,
-            msg : "Airplanes have been created",
-            data : airplane,
-            error : {}
-        });
+        SuccessResponse.data = airplane;
+        SuccessResponse.message = "Airplanes have been created";
+
+        return res.status(StatusCodes.CREATED).json(SuccessResponse);
     } catch (error) {
-        res.status(StatusCodes.INTERNAL_SERVER_ERROR).json({
-            success : false,
-            msg : "Something went wrong while creating airplane",
-            error : error
-        });
+        ErrorResponse.message = "Something went wrong while creating Airplane";
+        ErrorResponse.error = error;
+        res.status(StatusCodes.INTERNAL_SERVER_ERROR).json(ErrorResponse);
         
     }
 }
